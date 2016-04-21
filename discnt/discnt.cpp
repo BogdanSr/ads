424d7deb571264882c813430bb3f8d4a494b21d7
Bogdan Shubravyi <Bogdan.Shubravyi@eleks.com>
2016-04-21 10:58:42 +0300

    Changed "DISCNT" to work work with modified quick sort

--- discnt/discnt.cpp
+++ discnt/discnt.cpp
@@ -1,4 +1,12 @@
 //
+//  main_dscnt.cpp
+//  Numbers
+//
+//  Created by Bogdan Shubravyi on 4/21/16.
+//  Copyright © 2016 Bogdan Shubravyi. All rights reserved.
+//
+
+//
 //  main.cpp
 //  Numbers
 //
@@ -12,6 +20,8 @@
 #include <vector>
 #include <string.h>
 #include <iomanip>
+
+#include <unistd.h>
 
 using namespace std;
 
@@ -31,31 +41,39 @@ double calculateSum(vector<int> *numbers, int divider, double percentage)
     {
         result += numbers->at(i);
     }
+    
     return result;
 }
 
-void sortItems(vector<int> *numbers, int divider)
+void quick_sort(vector<int> *numbers, int left, int right, int stop_index)
 {
-    long n = numbers->size();
-    long max_elements = n / divider;
-    
-    for (int i = 0; i < max_elements; i++)
-    {
-        int iMin = i;
-        
-        for (int j = i + 1; j < n; j++)
-        {
-            if (numbers->at(j) > numbers->at(iMin)) {
-                iMin = j;
-            }
-        }
-        
-        if (i != iMin) {
-            swap(numbers->at(i), numbers->at(iMin));
-        }
+    if (left > stop_index || right < stop_index) {
+        return;
     }
+    
+    int i = left, j = right;
+    int tmp;
+    int pivot = numbers->at((left + right) / 2);
+    
+    while (i <= j)
+    {
+        while (numbers->at(i) > pivot) i++;
+        while (numbers->at(j) < pivot) j--;
+        
+        if (i <= j) {
+            tmp = numbers->at(i);
+            numbers->at(i) = numbers->at(j);
+            numbers->at(j) = tmp;
+            i++;
+            j--;
+        }
+    };
+    
+    if (left < j)
+        quick_sort(numbers, left, j, stop_index);
+    if (i < right)
+        quick_sort(numbers, i, right, stop_index);
 }
-
 
 int main(int argc, const char * argv[])
 {
@@ -64,41 +82,31 @@ int main(int argc, const char * argv[])
     ifstream in("discnt.in",ios::in);
     
     int number;
+    
     while (in >> number) {
         numbers->push_back(number);
     }
-    
     in.close();
     
     int discount = numbers->at(numbers->size() - 1);
     numbers->pop_back();
     
     const int divider = 3;
+    int stop_index = (int)numbers->size() / divider;
+    int n = (int)numbers->size() - 1;
     
-    sortItems(numbers, divider);
+    quick_sort(numbers, 0, n, stop_index);
     
     double result = calculateSum(numbers, divider, 1.0 - (discount / 100.0));
     
     ofstream output;
     output.open ("discnt.out");
     output << setprecision(2) << fixed << result;
+    
     output.close();
+    
+    cin.get();
     
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
